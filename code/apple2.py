8c3b5360697ebe2d1239eb104470fd130ed68aa5
Cactuz <you@example.com>
2023-10-21 19:39:26 +0200

    Apple animations

--- code/apple2.py
+++ code/apple2.py
@@ -1,5 +1,6 @@
 from settings import *
 from random import choice
+from math import sin
 
 
 class Apple:
@@ -11,9 +12,7 @@ class Apple:
 
         # use join method for the non-linux users out there
         # apparently needs to omit ".." because the path is relative to main.py
-        self.surface = pygame.image.load(
-            join("graphics", "apple.png")
-        ).convert_alpha()
+        self.surf = pygame.image.load(join("graphics", "apple.png")).convert_alpha()
 
     def set_pos(self):
         # basically the pygame.Vector2(x, y) basically gets stored in a list where you have both x and y posistion and check if apple is on the snake body
@@ -27,9 +26,12 @@ class Apple:
         self.pos = choice(available_pos)
 
     def draw(self):
-        self.display_surface.blit(
-            self.surface,
-            pygame.Rect(
-                self.pos.x * CELL_SIZE, self.pos.y * CELL_SIZE, CELL_SIZE, CELL_SIZE
+        scale = 1.5 + sin(pygame.time.get_ticks() / 600) / 5
+        self.scaled_surf = pygame.transform.smoothscale_by(self.surf, scale)
+        self.scaled_rect = self.scaled_surf.get_rect(
+            center=(
+                self.pos.x * CELL_SIZE + CELL_SIZE / 2,
+                self.pos.y * CELL_SIZE + CELL_SIZE / 2,
             )
         )
+        self.display_surface.blit(self.scaled_surf, self.scaled_rect)
