1fce30615b91de4b82f06f6d5fd1327b3163c400
Cactuz <you@example.com>
2023-10-21 19:20:16 +0200

    add apple.png surface

--- code/apple2.py
+++ code/apple2.py
@@ -1,6 +1,7 @@
 from settings import *
 from random import choice
 
+
 class Apple:
     def __init__(self, snake):
         self.pos = pygame.Vector2()
@@ -8,11 +9,27 @@ class Apple:
         self.snake = snake
         self.set_pos()
 
+        # use join method for the non-linux users out there
+        # apparently needs to omit ".." because the path is relative to main.py
+        self.surface = pygame.image.load(
+            join("graphics", "apple.png")
+        ).convert_alpha()
+
     def set_pos(self):
         # basically the pygame.Vector2(x, y) basically gets stored in a list where you have both x and y posistion and check if apple is on the snake body
         # add the coordinates to list of available positions if it isn't on the body
-        available_pos = [pygame.Vector2(x, y) for x in range(COLS) for y in range(ROWS) if pygame.Vector2(x, y) not in self.snake.body]
+        available_pos = [
+            pygame.Vector2(x, y)
+            for x in range(COLS)
+            for y in range(ROWS)
+            if pygame.Vector2(x, y) not in self.snake.body
+        ]
         self.pos = choice(available_pos)
 
     def draw(self):
-        pygame.draw.rect(self.display_surface, 'red', pygame.Rect(self.pos.x * CELL_SIZE, self.pos.y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+        self.display_surface.blit(
+            self.surface,
+            pygame.Rect(
+                self.pos.x * CELL_SIZE, self.pos.y * CELL_SIZE, CELL_SIZE, CELL_SIZE
+            )
+        )
